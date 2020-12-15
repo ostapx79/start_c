8dbf51af4735c616422d12987825038a84a1399e
Constantin Galus <ostapx79@gmail.com>
2020-12-16 02:21:10 +0300

    д/з по c_cadr, урок 8 упражнение 1, не готовая программа но рабочая

--- c_cadr/lesson8/homework/homework8_1.c
+++ c_cadr/lesson8/homework/homework8_1.c
@@ -2,7 +2,24 @@
  * c_cadr/lesson8/homework8/homework8_1.c
  * Домашнее задание:
  * 0. Доделать елочку.
- * 1. Разработка аналог утилиты ws.
+ * 1. Разработка аналог утилиты wc.
  * 2. Сделать подсчет статистике по всем символам ASCII.
  */
- 
+ #include <stdio.h>
+
+int main(int argc, char** argv) {
+    int wc;
+    int digits_wc = 0; // цифры
+    int chars_wc = 0; // символы
+    int words_wc = 0; // слова
+    int space_wc = 0; // пробелы
+    int tabs_wc = 0; // табуляции
+    int newline_wc = 0; // новые строки
+    int total_wc = 0; 
+
+    while ((wc = getchar()) != EOF) {
+	++total_wc;
+    }
+    printf("total_wc -> %d\n", total_wc);
+    return 0;
+}
