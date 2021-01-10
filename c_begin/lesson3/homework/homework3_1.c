eff3d9b25eceaadeac805548ffe2a960fd18aceb
ConstantinGalus <ostapx79@gmail.com>
2021-01-10 16:50:26 +0300

    д/з по begin_c, урок 3 упражнение 1

---
+++ c_begin/lesson3/homework/homework3_1.c
@@ -0,0 +1,21 @@
+/* Изменить программу так, чтобы если условия если условия t > 10, не
+ * выполняется т.е. ложное. Но программа кроме вывода ggg завершилась с
+ * результатом 7.
+ * start_c/begin_c/lesson3/homework/homework3_1.c
+ */
+#include <stdio.h>
+
+int main() {
+    int t;
+    t = 5;
+
+    if (t > 10) {
+        printf("www\n");
+        if(t > 20) {
+            printf("123\n");
+        }
+    } else {
+        printf("ggg\n");
+    }
+    return 0;
+}
