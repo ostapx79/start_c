9dca0be6e78ffe54f4a33d352581f3a079197c21
ConstantinGalus <ostapx79@gmail.com>
2021-01-10 16:35:57 +0300

    условные операторы

---
+++ c_begin/lesson3/lesson3_1.c
@@ -0,0 +1,19 @@
+/* Условные операторы 
+ * start_c/begin_c/lesson3/lesson3_1.c
+ */
+#include <stdio.h>
+
+int main() {
+    int t;
+    t = 5;
+
+    if (t > 10) {
+        printf("www\n");
+        if (t > 20) {
+            printf("123\n");
+        }
+    } else {
+        printf("ggg\n");
+    }
+    return 0;
+}
