ccd25aee755ff3e626dd26546b3aaaa8d3905cd5
ConstantinGalus <ostapx79@gmail.com>
2021-01-10 17:05:21 +0300

    д/з по begin_c, урок 3 упражнение 2

---
+++ c_begin/lesson3/homework/homework3_2.c
@@ -0,0 +1,22 @@
+/* Изменить программу так, чтобы если t > 30, то программа кроме www123
+ * напечатала еще какие-нибудь 3 символа и значение переменной t.
+ * start_c/begin_c/lesson3/homework/homework3_2.c
+ */
+#include <stdio.h>
+
+int main() {
+    int t = 35;
+
+    if (t > 10) {
+        printf("www\n");
+        if (t > 20) {
+            printf("123\n");
+            if (t > 30) {
+                printf("ddd\nt-%d\n", t);
+            }
+        }
+    } else {
+        printf("ggg\n");
+    }
+    return 0;
+}
