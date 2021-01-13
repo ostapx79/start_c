4ae729e81868bffb8fba1c8cf7303e6a7cebb8d9
ConstantinGalus <ostapx79@gmail.com>
2021-01-13 16:00:03 +0300

    циклы и массивы в Си с c_begin

---
+++ c_begin/lesson6/lesson6_1.c
@@ -0,0 +1,15 @@
+/* start_c/c_begin/lesson6/lesson6_1.c
+ * циклы и массивы в Си и Си++.
+ */
+
+#include <stdio.h>
+
+int main() {
+    int numb_i = 10;
+    int n_i = 1;
+    while (n_i < numb_i) {
+        printf("Hello, termux in C programming!\n");
+        ++n_i;
+    }
+    return 0;
+}
---
+++ c_begin/lesson6/lesson6_2.c
@@ -0,0 +1,17 @@
+/* start_c/c_begin/lesson6/lesson6_2.c
+ * Массивы (array).
+ */
+
+#include <stdio.h>
+
+int main() {
+    int mas[10];
+    mas[0] = 0;
+    mas[1] = 1;
+    mas[2] = 2;
+
+    printf("в mas[0] -> %d\n", mas[0]);
+    printf("в mas[1] -> %d\n", mas[1]);
+    printf("в mas[2] -> %d\n", mas[2]);
+    return 0;
+}
