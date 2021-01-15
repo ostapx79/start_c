4f75564c26bf7acb9b57f7f547d474efbe3a12c1
ConstantinGalus <ostapx79@gmail.com>
2021-01-15 20:02:21 +0300

    д/з по Си с c_begin, символы и строки в Си и Си++

---
+++ c_begin/lesson7/lesson7_1.c
@@ -0,0 +1,15 @@
+/* start_c/c_begin/lesson7/lesson7_1.c
+ * Символы и строки в Си и Си++.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char ch = 'W';
+    printf("ch - %c\n", ch);
+
+    scanf("%c", &ch);
+    printf("ch-1 - %c\n", ch);
+
+    return 0;
+}
