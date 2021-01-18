e645dcd9628c7e6fc2beefdda845f647649a3202
ConstantinGalus <ostapx79@gmail.com>
2021-01-18 18:24:57 +0300

    цикл for

---
+++ c_begin/lesson8/lesson8_3.c
@@ -0,0 +1,13 @@
+/*
+ * start_c/c_begin/lesson8/lesson8_3.c
+ * Цикл for
+ */
+#include <stdio.h>
+
+int main() {
+    int numb_i;
+    for (numb_i = 10; numb_i > 0; numb_i--) {
+        printf("%d\n", numb_i);
+    }
+    return 0;
+}
