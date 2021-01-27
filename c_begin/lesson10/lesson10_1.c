b26b2be2607fa8eef1d3a279c6c85b0ae7e9b55d
ConstantinGalus <ostapx79@gmail.com>
2021-01-27 17:54:54 +0300

    убрали личные пробеды, и один добавили

--- c_begin/lesson10/lesson10_1.c
+++ c_begin/lesson10/lesson10_1.c
@@ -8,17 +8,15 @@
 #include <unistd.h>
 
 int mygetch() {
-
     struct termios oldt, newt;
     int ch;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    newt.c_lflag &= ~(ICANON |ECHO);
+    newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
     ch = getchar();
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
-    
 }
 
 int main() {
