77e0bd9c7b0056884f89f654a29069bd7488250c
ConstantinGalus <ostapx79@gmail.com>
2021-02-11 16:08:39 +0300

    преопределение типа в Си и Си++ с помощию typedef, с c_begin

---
+++ c_begin/lesson12/lesson12_3.c
@@ -0,0 +1,15 @@
+/*
+ * start_c/c_begin/lesson12/lesson12_3.c
+ * Переопределения типа в Си и Си++.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int celoe;
+
+int main() {
+    celoe numb_c = 32;
+
+    printf("celoe -> %d\n", numb_c);
+    return 0;
+}
